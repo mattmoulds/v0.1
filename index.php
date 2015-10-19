087374a314a0438c30d30d0522f5e1615bf54741
matthew.moulds1@gmail.com <matthew.moulds1@gmail.com>
2015-10-19 22:28:09 +0100

    css and html changes

--- index.php
+++ index.php
@@ -5,25 +5,17 @@
 	Matthew Moulds
   </title>
    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <script src="js/jquery.min.js"></script>
-    <script src="js/skel.min.js"></script>
-    <script src="js/init.js"></script>
-   <noscript>
-    <link rel="stylesheet" href="css/skel.css" />
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/style-wide.css" />
-   </noscript>
+    <link rel="stylesheet" href="css/style_1.css">
  </head>
-
- <body class="index loading">	
-  <section id="banner">		
+ <body class="index loading">
+  <section id="banner">
    <div class="inner">
     <?php
      include('connect-db.php');
      $result = mysql_query("SELECT name,job_title FROM info order by id desc limit 1")
      or die(mysql_error());
      while($row = mysql_fetch_array( $result )) {
-     echo "<br><header><h2>". $row["name"]. "</header></h2>". "<p>". $row["job_title"] . "</strong></p>";
+     echo "<header><h2>". $row["name"]. "</header></h2>". "<p>". $row["job_title"] . "</strong></p>";
      }
     ?>
 
@@ -34,13 +26,13 @@
     </ul>
    </div>
   </section>
-		
+
   <article id="main">
-   <header class="special container">				
+   <header class="special container">
     <h2>
-     <strong> 
+     <strong><p>
 	Welcome
-     </strong>
+  </p></strong>
     </h2>
 
     <?php
@@ -49,13 +41,9 @@
      $result = mysql_query("SELECT blog_header,blog_short,id FROM blog order by id desc limit 20")
      or die(mysql_error());
      while($row = mysql_fetch_array( $result )) {
-     echo '<br><h3><a href="id.php?id=' . $row["id"] .'">' . $row["blog_header"]. "</h3></a>". "<div id='blog_p'>". $row["blog_short"] . "</div>";
+     echo '<br><h3><a href="id.php?id=' . $row["id"] .'">' . $row["blog_header"]. "</h3></a>". "<h4><div id='blog_p'>". $row["blog_short"] . "</div></h4>";
      }
     ?>
-   </header>	
-
-   <footer id="footer">
-   </footer>
-
+   </header>
  </body>
 </html>
